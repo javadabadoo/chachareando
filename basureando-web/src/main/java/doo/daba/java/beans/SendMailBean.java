d5ea923314b51ea22ad1370094ddd9db1b1824f6
javadabadoo <gerardo.agalicia@gmail.com>
2013-06-11 17:57:22 -0500

    Bean de prueba para validacion

--- basureando-web/src/main/java/doo/daba/java/beans/SendMailBean.java
+++ basureando-web/src/main/java/doo/daba/java/beans/SendMailBean.java
@@ -1,8 +1,12 @@
 package doo.daba.java.beans;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Email;
+
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
@@ -10,10 +14,17 @@ import javax.validation.constraints.Size;
  * User: Gerardo Aquino
  * Date: 10/06/13
  */
+@ToString
 public class SendMailBean {
 
     @NotNull
-    @Size(max = 100)
-    @Pattern(regexp="^([a-zA-Z0-9\\-\\.\\_]+)'+'(\\@)([a-zA-Z0-9\\-\\.]+)'+'(\\.)([a-zA-Z]{2,4})$")
+    @Size(max = 100, message = "La longitud del correo es muy laraga")
+    @Email(message = "Dirección de correo incorrecta")
+    @Getter @Setter
     private String sentFrom;
+
+    @NotNull
+    @Size(min = 5, max = 50, message = "Longitud inválida")
+    @Getter @Setter
+    private String title;
 }
