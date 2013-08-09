3e9c1bb0789e941a17946f6a982f298ef578efe7
javadabadoo <gerardo.agalicia@gmail.com>
2013-08-09 15:07:32 -0500

    Eliminando proyecto touch
    Eliminamos el proyecto basureando-touch porque no tengo planeado nada
    con el

---
+++ basureando-beans/src/main/java/doo/daba/java/beans/TwitterSession.java
@@ -0,0 +1,9 @@
+package doo.daba.java.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gerardo Aquino
+ * Date: 9/08/13
+ */
+public class TwitterSession {
+}
---
+++ basureando-exception/src/main/java/doo/daba/java/exceptions/twitter/TwitterException.java
@@ -0,0 +1,9 @@
+package doo.daba.java.exceptions.twitter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gerardo Aquino
+ * Date: 9/08/13
+ */
+public class TwitterException {
+}
